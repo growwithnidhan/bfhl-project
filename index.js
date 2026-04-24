bba6bf8855322ed3b6483b53f70421a04c0e9bea
growwithnidhan <mail2nidhan@gmail.com>
2026-04-24 19:14:25 +0530

    frontend updated

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname)));
 
 const USER_ID = "nidhanayoob_23112004";
 const EMAIL_ID = "na3175@srmist.edu.in";
@@ -188,6 +190,10 @@ app.post('/bfhl', (req, res) => {
       largest_tree_root
     }
   });
+});
+
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
